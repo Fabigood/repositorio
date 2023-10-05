2a2a06b04934b6bbe716f0eeeb9beac94e36bffb
Fabio Gonzalez <139414666+Fabigood@users.noreply.github.com>
2023-10-05 16:51:29 -0500

    Update numeroprimo.c
    final

--- numeroprimo.c
+++ numeroprimo.c
@@ -4,6 +4,26 @@ int main(){
 
     printf("Ingrese un numero: ");
     scanf("%d",&num);
+    if(num!=-1 && num>0)
+        {
+            primo=0;
+            div=2;
+            while(div<num  && primo!=1)
+            {
+                if(num%div==0) primo=1;
+                div++;
+            }
+            if (primo==0)
+            {
+                printf("\nEl numero %d si es primo",num);
+              }
+            else
+            {
+                printf("\nEl numero %d no es primo",num);
+            }
+        }
+    } while(num!=-1);
+    
     
 
 
